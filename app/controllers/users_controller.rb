211617dbf19fe3af4df38bddd8bb160ec40e9e82
Khanya Keswa <khanya.keswa@gmail.com>
2022-12-29 17:38:32 -0600

    login and auth funtional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
     def index
         render json: User.all
     end
+    
+    def show 
+        render json: current_user, status: :ok
+    end 
 
     def create
         render json: User.create!(user_params), status: :created
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_22_174933) do
+ActiveRecord::Schema.define(version: 2022_12_29_062204) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2022_12_22_174933) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
